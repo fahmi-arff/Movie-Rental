9bb57bb48cc76fe15b104769994cbcbd5cd196c0
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-03 20:00:29 +0700

    add route parameter on express

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/api/courses', (req, res) => {
   res.send([1,2,3]);
 });
 
+//  /2018/1 in input
+app.get('/api/posts/:year/:month', (req, res) => {
+  res.send(req.params);
+});
+
 // export PORT=5000 on terminal
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
