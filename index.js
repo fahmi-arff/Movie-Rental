3554cce2074e2fa863a69b7c739f41d56b8b781b
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-03 20:12:07 +0700

    add route on courses id with object data

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const express = require('express');
 // Create Express application by convenction call app
 const app = express();
 
+const courses = [
+  { id: 1 , name: 'course1' },
+  { id: 2 , name: 'course2' },
+  { id: 3 , name: 'course3' }
+]
+
 // callback as route handler
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send([1,2,3]);
+  res.send(courses);
 });
 
-//  /2018/1 in input
-app.get('/api/posts/:year/:month', (req, res) => {
-  res.send(req.params);
+app.get('/api/courses/:id', (req, res) => {
+  // need to parseInt because id is return object
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  // if course not found return 404
+  if(!course) res.status(404).send('The course with given ID was not found');
+  res.send(course);
 });
 
 // export PORT=5000 on terminal
