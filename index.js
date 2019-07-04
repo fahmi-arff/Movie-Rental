9292835fe279440d316a05201697826e0bf4df69
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-04 20:19:47 +0700

    add put request

--- index.js
+++ index.js
@@ -31,13 +31,9 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req,res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -48,6 +44,29 @@ app.post('/api/courses', (req,res) => {
   courses.push(course);
   res.send(course);
 })
+
+app.put('/api/courses/:id', (req,res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if(!course) res.status(404).send('The course with given ID was not found');
+
+  // const result = validateCourse(req.body); non destructive
+  const { error } = validateCourse(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+})
+
+function validateCourse(course){
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+}
 
 // export PORT=5000 on terminal
 const port = process.env.PORT || 3000;
