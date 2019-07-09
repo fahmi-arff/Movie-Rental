b4d1f765fdb2b07a7831c6148f1767f08a700bc9
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-09 13:11:52 +0700

    separate model schema from routes genres

--- routes/genres.js
+++ routes/genres.js
@@ -1,16 +1,7 @@
-const Joi = require('@hapi/joi');
+const {Genre, validate} = require('../models/genre');
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50
-  }
-}));
 
 router.get('/', async(req, res) => {
   const genres = await Genre.find().sort('name');
@@ -24,7 +15,7 @@ router.get('/:id', async(req, res) => {
 });
 
 router.post('/', async(req,res) => {
-  const { error } = validateGenres(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let genre = new Genre({ name: req.body.name });
@@ -33,7 +24,7 @@ router.post('/', async(req,res) => {
 })
 
 router.put('/:id', async(req,res) => {
-  const { error } = validateGenres(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findByIdAndUpdate(
@@ -52,13 +43,5 @@ router.delete('/:id', async(req, res) => {
 
   res.send(genre);
 })
-
-function validateGenres(genre){
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  return Joi.validate(genre, schema);
-}
 
 module.exports = router;
