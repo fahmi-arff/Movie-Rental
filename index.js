086c9ef917be202b1d7a9cf183c557c464ef88d6
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-09 14:06:33 +0700

    add movie routes and separate genreSchema to const

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true, useFindAndModify: false})
@@ -11,6 +12,7 @@ mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true, useFi
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
 
 // export PORT=5000 on terminal
 const port = process.env.PORT || 3000;
