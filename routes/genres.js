9ad0a63b6eab8590d80f3c3c3c056f685edd2f24
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-11 06:51:34 +0700

    move validating object id to middleware

--- routes/genres.js
+++ routes/genres.js
@@ -1,3 +1,4 @@
+const validateObjectId = require('../middleware/validateObjectId');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const {Genre, validate} = require('../models/genre');
@@ -10,10 +11,7 @@ const genres = await Genre.find().sort('name');
   res.send(genres);
 });
 
-router.get('/:id', async(req, res) => {
-if(!mongoose.Types.ObjectId.isValid(req.params.id))
-  return res.status(404).send('Invalid ID.');
-  
+router.get('/:id', validateObjectId, async(req, res) => {
 const genre = await Genre.findById(req.params.id);
 
 if(!genre) return res.status(404).send('The genre with given ID was not found');
