cc447d9a084f37dcdfdde2841f6e88e60b551f1d
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-11 06:29:48 +0700

    test genre id with parameter

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -7,7 +7,7 @@ describe('/api/genres', () => {
   beforeEach(() => { server = require('../../index'); })
   afterEach(async() => { 
     server.close();
-    await Genre.deleteOne({});
+    await Genre.remove({});
   })
 
   describe('GET /', () => {
@@ -23,6 +23,15 @@ describe('/api/genres', () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
       expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+    }),
+    it('should return a genre if valid id is passed', async () => {
+      const genre = new Genre({ name: 'genre1' });
+      await genre.save();
+
+      const res = await request(server).get('/api/genres/' + genre._id);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
     })
   })
 })
