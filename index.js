6f174a02ee5555b5bc1e6f812a03cec9aa858639
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-10 12:01:28 +0700

    extracting db logic to db startup

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const Joi = require('@hapi/joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 
 require('./startup/routes')(app);
+require('./startup/db')();
 
 winston.handleExceptions(
   new winston.transports.File({ filename: 'uncaughtExceptions.log' })
@@ -21,20 +21,10 @@ process.on('unhandledRejection', ex => {
 winston.add(winston.transports.File, { filename: 'logfile.log' });
 winston.add( winston.transports.MongoDB, { db: 'mongodb://localhost/playground' });
 
-const p = Promise.reject(new Error('Something failed miserably!'));
-p.then(() => console.log('Done'));
-
 if(!config.get('jwtPrivateKey')){
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
   process.exit(1);
 }
 
-mongoose.connect('mongodb://localhost/playground', {useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true})
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.log('Could not connect to MongoDB..'))
-
-
-
-// export PORT=5000 on terminal
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));
