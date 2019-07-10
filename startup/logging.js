2645e01bc9414401f6e4a2becb1c8354b6ce2361
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-10 13:00:37 +0700

    showing unhandled exception on the console

--- startup/logging.js
+++ startup/logging.js
@@ -4,7 +4,8 @@ require('express-async-errors');
 
 module.exports = function() {
   winston.handleExceptions(
-    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' }),
+    new winston.transports.Console({ colorize: true, prettyPrint: true })
   )
   
   process.on('unhandledRejection', ex => {
