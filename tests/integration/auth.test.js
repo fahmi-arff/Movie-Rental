0202a1a71eff329a79f7874bbdd96a3071c544c3
Fahmi Arief <fahmi.arff@gmail.com>
2019-07-11 19:29:33 +0700

    add complete integrating test on auth

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const {User} = require('../../models/user');
 const {Genre} = require('../../models/genre');
+const bcrypt = require('bcrypt');
 
 let server;
 let token;
@@ -40,6 +41,85 @@ describe('auth middleware', () => {
   
   it('should return 200 if token is valid', async() => {
     const res = await exec();
+
+    expect(res.status).toBe(200);
+  })
+})
+
+describe('auth user login', () => {
+  beforeEach(() => { server = require('../../index'); })
+  afterEach(async() => { 
+    server.close();
+    await User.remove({});
+  })
+  
+  let email;
+  let password;
+
+  const exec = async() => {
+    return await request(server)
+      .post('/api/users')
+      .send({ name:'name1', email, password});
+  }
+
+  const auth = async() => {
+    return await request(server)
+      .post('/api/auth')
+      .send({ email, password})
+  }
+
+  beforeEach(async() => {
+    email = 'name@email.com'
+    password = '12345'
+    await exec();
+  })
+
+  it('should return 400 if email is less than 5 characters', async() => {
+    email = "imel"
+
+    const res = await auth();
+
+    expect(res.status).toBe(400);
+  })
+  
+  it('should return 400 if email not right format', async() => {
+    email = 'thisEmail'
+    
+    const res = await auth()
+
+    expect(res.status).toBe(400);
+  })
+  
+  it('should return 400 if email is invalid', async() => {
+    email = 'fahmi@gmail.com';
+    const res = await auth()
+    
+    await User.findOne({ email });
+    
+    expect(res.status).toBe(400);
+  })
+  
+  it('should return 400 if password is less than 5 characters', async() => {
+    password = "1234"
+    
+    const res = await auth()
+
+    expect(res.status).toBe(400);
+  })
+
+  it('should return 400 if password is invalid', async() => {
+    password = "12346"
+
+    const res = await auth()
+    const user = await User.findOne({ email });
+    
+    await bcrypt.compare(password, user.password);
+
+    expect(res.status).toBe(400);
+  })
+
+  it('should return 200 if login sucess', async() => {
+    const res = await auth()
 
     expect(res.status).toBe(200);
   })
